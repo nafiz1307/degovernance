c01c368c96793594ba0cec599a8614afb30a3564
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2021-07-03 19:16:50 +0600

    Update LandingPage.js

--- src/views/examples/LandingPage.js
+++ src/views/examples/LandingPage.js
@@ -170,122 +170,29 @@ export default function LandingPage() {
               src={require("assets/img/path4.png").default}
             />
             <Container>
-              <Row className="row-grid justify-content-between">
-                <Col className="mt-lg-5" md="5">
-                  <Row>
-                    <Col className="px-2 py-2" lg="6" sm="12">
+            <Row className="row-grid justify-content-between">
+                    <Col className="px-2 py-2" lg="6" sm="10">
                       <Card className="card-stats">
                         <CardBody>
                           <Row>
-                            {/* <Col md="4" xs="5">
+                            <Col md="4" xs="5">
                               <div className="icon-big text-center icon-warning">
-                                <i className="tim-icons icon-trophy text-warning" />
+                                <i className="tim-icons icon-minimal-right text-info" />
                               </div>
-                            </Col> */}
-                            <Col md="11" xs="7">
+                            </Col>
+                            <Col md="8" xs="7">
                               <div className="numbers">
-                                {console.log(web3Context)}
-                                 <CardTitle tag="p">{account}</CardTitle>
+                              <CardTitle tag="p">{account}</CardTitle>
                                 <p />
-                                {/* <p className="card-category">Available Fungible Tokens</p> */}
+                                <p className="card-category">Connected Account</p>
                               </div>
                             </Col>
                           </Row>
                         </CardBody>
                       </Card>
                     </Col>
-                    {/* <Col className="px-2 py-2" lg="6" sm="12">
-                      <Card className="card-stats upper bg-default">
-                        <CardBody>
-                          <Row>
-                            <Col md="4" xs="5">
-                              <div className="icon-big text-center icon-warning">
-                                <i className="tim-icons icon-coins text-white" />
-                              </div>
-                            </Col>
-                            <Col md="8" xs="7">
-                              <div className="numbers">
-                                <CardTitle tag="p">3,653</CardTitle>
-                                <p />
-                                <p className="card-category">Commits</p>
-                              </div>
-                            </Col>
-                          </Row>
-                        </CardBody>
-                      </Card>
-                    </Col> */}
-                  </Row>
-                  <Row>
-                    <Col className="px-2 py-2" lg="8" sm="12">
-                      <Card className="card-stats">
-                        <CardBody>
-                          <Row>
-                            <Col md="4" xs="5">
-                              <div className="icon-big text-center icon-warning">
-                                <i className="tim-icons icon-coins text-white" />
-                              </div>
-                            </Col>
-                            <Col md="8" xs="7">
-                              <div className="numbers">
-                                <CardTitle tag="p">5980834</CardTitle>
-                                <p />
-                                <p className="card-category">Available</p>
-                              </div>
-                            </Col>
-                          </Row>
-                        </CardBody>
-                      </Card>
-                    </Col>
-                    {/* <Col className="px-2 py-2" lg="6" sm="12">
-                      <Card className="card-stats">
-                        <CardBody>
-                          <Row>
-                            <Col md="4" xs="5">
-                              <div className="icon-big text-center icon-warning">
-                                <i className="tim-icons icon-credit-card text-success" />
-                              </div>
-                            </Col>
-                            <Col md="8" xs="7">
-                              <div className="numbers">
-                                <CardTitle tag="p">10,783</CardTitle>
-                                <p />
-                                <p className="card-category">Forks</p>
-                              </div>
-                            </Col>
-                          </Row>
-                        </CardBody>
-                      </Card>
-                    </Col> */}
-                  </Row>
-                </Col>
-                <Col className="mt-lg-5" md="5">
-                  {/* <div className="pl-md-5">
-                    <h1>
-                      Large <br />
-                      Achivements
-                    </h1>
-                    <p>
-                      I should be capable of drawing a single stroke at the
-                      present moment; and yet I feel that I never was a greater
-                      artist than now.
-                    </p>
-                    <br />
-                    <p>
-                      When, while the lovely valley teems with vapour around me,
-                      and the meridian sun strikes the upper surface of the
-                      impenetrable foliage of my trees, and but a few stray.
-                    </p>
-                    <br />
-                    <a
-                      className="font-weight-bold text-info mt-5"
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      Show all{" "}
-                      <i className="tim-icons icon-minimal-right text-info" />
-                    </a>
-                  </div> */}
-                  <Table>
+                    <Col className="px-2 py-2" lg="4" sm="12">
+                      <Table>
                     <caption className="text-center">List of NFT & FT</caption>
                     <thead>
                       <tr>
@@ -317,9 +224,65 @@ export default function LandingPage() {
                         <td>NFT</td>
                         <td className="text-center">2015</td>
                         <td className="text-right">22043</td>
+                        
                       </tr>
                     </tbody>
                   </Table>
+                    </Col>
+                  </Row>
+              <Row className="row-grid justify-content-between">
+                <Col>
+                  <Row>
+                    <Col className="px-2 py-2" lg="8" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-coins text-white" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">5980834</CardTitle>
+                                <p />
+                                <p className="card-category">Available</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    
+                  </Row>
+                </Col>
+                <Col className="mt-lg-5" md="5">
+                  {/* <div className="pl-md-5">
+                    <h1>
+                      Large <br />
+                      Achivements
+                    </h1>
+                    <p>
+                      I should be capable of drawing a single stroke at the
+                      present moment; and yet I feel that I never was a greater
+                      artist than now.
+                    </p>
+                    <br />
+                    <p>
+                      When, while the lovely valley teems with vapour around me,
+                      and the meridian sun strikes the upper surface of the
+                      impenetrable foliage of my trees, and but a few stray.
+                    </p>
+                    <br />
+                    <a
+                      className="font-weight-bold text-info mt-5"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      Show all{" "}
+                      <i className="tim-icons icon-minimal-right text-info" />
+                    </a>
+                  </div> */}
                 </Col>
               </Row>
             </Container>
